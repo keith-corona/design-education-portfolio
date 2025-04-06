5aa4cdfbeb81e13c04558afad70543c31ea2b440
Keith Corona <keithcorona23@gmail.com>
2025-04-05 21:54:08 -0700

    font size and underline

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,10 @@
-<main class="flex flex-col flex-1 p-4 lg:mx-10">
+<main class="flex flex-col flex-1 justify-items-center p-4 lg:mx-10">
   <h1
-    class="text-cyan-700 text-3xl sm:text-6xl md:text-6xl mb-6"
+    class="text-cyan-700 text-xl sm:text-6xl md:text-6xl mb-6"
     style="line-height:1.1;"
   >
     Sorry, but this page cannot be found. Please navigate back to the <a
-      class="font-bold"
+      class="font-bold underline"
       href="/">home page</a
     > or try again.
   </h1>
