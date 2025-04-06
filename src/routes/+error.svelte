aad0def187876574b8d9843ec86c4f91c5616bd6
Keith Corona <keithcorona23@gmail.com>
2025-04-05 17:35:18 -0700

    styling on link for error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,10 @@
 <main class="flex flex-col flex-1 p-4 lg:mx-10">
   <h1
-    class="text-cyan-700 font-bold text-3xl sm:text-6xl md:text-6xl mb-6"
+    class="text-cyan-700 text-3xl sm:text-6xl md:text-6xl mb-6"
     style="line-height:1.1;"
   >
     Sorry, but this page cannot be found. Please navigate back to the <a
+      class="font-bold"
       href="/">home page</a
     > or try again.
   </h1>
