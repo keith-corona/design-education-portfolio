c9b2e77f63333ac145c266c8fd52913f3fd61745
Keith Corona <keithcorona23@gmail.com>
2025-04-05 17:31:05 -0700

    Updated error styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,10 @@
-<h1>
-  Sorry, but this page cannot be found. Please navigate back to the <a href="/"
-    >home page</a
-  > or try again.
-</h1>
+<main class="flex flex-col flex-1 p-4 lg:mx-10">
+  <h1
+    class="text-cyan-700 font-bold text-3xl sm:text-6xl md:text-6xl mb-6"
+    style="line-height:1.1;"
+  >
+    Sorry, but this page cannot be found. Please navigate back to the <a
+      href="/">home page</a
+    > or try again.
+  </h1>
+</main>
